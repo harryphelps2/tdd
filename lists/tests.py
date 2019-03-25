065b2a5c239103e485fb76340eb7f1d45c9062f3
harryphelps <harry.phelps@octoenergy.com>
2019-03-25 08:15:37 +0000

    Add new template used test for existing lists

--- lists/tests.py
+++ lists/tests.py
@@ -6,12 +6,8 @@ from lists.views import home_page
 from lists.models import Item
 
 class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
     
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
     
@@ -21,9 +17,6 @@ class HomePageTest(TestCase):
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
-    
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
 
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})    
@@ -34,14 +27,22 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
+class ListViewCase(TestCase):
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+    def test_displays_all_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
+    
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
 
 class ItemModelTest(TestCase):
     def test_saving_and_retieving_items(self):
@@ -61,13 +62,4 @@ class ItemModelTest(TestCase):
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
 
-class ListViewCase(TestCase):
 
-    def test_displays_all_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-
-        response = self.client.get('/lists/the-only-list-in-the-world/')
-
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
